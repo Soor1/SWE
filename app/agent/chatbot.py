b10e650257863d7e34757bc8b7001ea121c218ba
adrianp-7 <aipavila7@gmail.com>
2025-04-21 20:16:42 -0400

    updated init function

--- app/agent/chatbot.py
+++ app/agent/chatbot.py
@@ -2,6 +2,11 @@ from hugchat import hugchat
 from hugchat.login import Login
 import os
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from data_pipeline.embedding.pinecone_interface import PineconeInterface
+
+
 
 
 # Reference: https://github.com/Soulter/hugging-chat-api
@@ -14,13 +19,16 @@ from dotenv import load_dotenv
 class RAGAgent:
     def __init__(self):
         """Initialize the RAGAgent class, hugchat client, and load environment variables."""
-        load_dotenv()
-        email = os.getenv("EMAIL")
-        password = os.getenv("PASSWD")
-        cookie_path_dir = "./cookies/"
-        sign = Login(email, password)
-        cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
-        self.chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+        self.chatbot = None
+
+        self.pinecone_interface = PineconeInterface()
+        assert self.pinecone_interface.check_connection()
+
+        uri = f"mongodb+srv://josephmolina:{os.environ.get('MONGO_PASSWORD')}@legislationchat.3brsn.mongodb.net/?appName=LegislationChat"
+        self.mongo_client = MongoClient(uri, server_api=ServerApi('1'))
+        res = self.mongo_client.admin.command('ping')
+        assert res.get("ok", 0) == 1
+        self.sign_in(os.environ.get("HUGGINGFACE_EMAIL"), os.environ.get("HUGGINGFACE_PASSWORD"))
 
     def retrieve_similar_question(self, user_query):
         """Finds the most similar question from the stored Q&A pairs and returns the embedding vector value, question, and answer.
