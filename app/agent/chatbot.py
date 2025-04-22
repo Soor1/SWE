fb4fd975491e1a0c899c80953b218e50300767e0
adrianp-7 <aipavila7@gmail.com>
2025-04-21 20:19:14 -0400

    added sign in function

--- app/agent/chatbot.py
+++ app/agent/chatbot.py
@@ -30,6 +30,23 @@ class RAGAgent:
         assert res.get("ok", 0) == 1
         self.sign_in(os.environ.get("HUGGINGFACE_EMAIL"), os.environ.get("HUGGINGFACE_PASSWORD"))
 
+    def sign_in(self, email, password):
+        """Sign in with the provided username and password."""
+        if self.chatbot is not None and self.email == email and self.password == password:
+            return True
+        try:
+            self.email = email
+            self.password = password
+            cookie_path_dir = "./cookies/"
+            sign = Login(email, password)
+            cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
+            self.chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+            return True
+        except Exception as e:
+            print(f"Error signing in: {e}")
+            self.chatbot = None
+            return False
+
     def retrieve_similar_question(self, user_query):
         """Finds the most similar question from the stored Q&A pairs and returns the embedding vector value, question, and answer.
         Focus on this in Sprint 1.
